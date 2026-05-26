64ef733d77894c0fe59fa312538a46c0aad8fd66
최 우성 <ws.choi@okekr.onmicrosoft.com>
2026-05-26 09:59:48 +0900

    26일업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx 파일에 들어갈 내용 (예시)
-import MyComponent from './okestro_academy_v2(20260508)'; // 다운받은 파일 이름으로 적어주세요. (확장자는 생략 가능)
+import MyComponent from './ApplicantManager';
 
 function App() {
   return (
