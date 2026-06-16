3ab61dd6f7de106d815f8b5bbbb93888d4db281d
letmesaywoosay <letmesaywoosay@gmail.com>
2026-06-16 18:29:24 +0900

    Production release at 2026. 6. 16. 오후 6:29:24

--- run-dev.bat
+++ run-dev.bat
@@ -1,4 +1,4 @@
 @echo off
 echo [TUNE LMS] 로컬 개발 서버를 실행합니다...
-"C:\Users\User\AppData\Local\ms-playwright-go\1.57.0\node.exe" "node_modules\vite\bin\vite.js"
+"C:\Users\User\AppData\Local\ms-playwright-go\1.57.0\node.exe" "node_modules\vite\bin\vite.js" --host
 pause
